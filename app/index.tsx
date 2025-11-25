364fb366b07c23f318ea8ae6b8b971e825fa156d
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-11-25 01:11:05 -0400

    feat: implement initial application structure with wallet, accounts, dashboard, and user management.

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ import IconAt from "@/svgs/dashboard/IconAt";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { ActivityIndicator, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Alert, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -26,7 +26,7 @@ export default function Index() {
         router.replace('/dashboard/dashboard')
       }
     }
-    
+
     checkAuth();
 
     return () => {
@@ -35,6 +35,11 @@ export default function Index() {
   }, [])
 
   const handleSubmit = async () => {
+    if (!credentials.email || !credentials.password) {
+      Alert.alert('Malet | Error', 'Por favor, completa todos los campos');
+      return;
+    }
+
     const r = await login(credentials);
     if (r) {
       router.replace('/dashboard/dashboard')
@@ -43,64 +48,64 @@ export default function Index() {
 
   return (
     <>
-    <SafeAreaView
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        width: '100%',
-      }}
-    >
-      <View style={{ marginBottom: 40 }}>
-        <IconAt style={{ width: 80, height: 80 }} />
-      </View>
-
-      <View style={{ width: '85%', justifyContent: 'center', alignItems: 'center', gap: 20 }}>
-        <View style={{ width: '100%', gap: 10 }}>
-          <TextMalet style={{ fontSize: 16 }}>
-            Correo Electrónico
-          </TextMalet>
-          <Input placeholder="tucorreo@ejemplo.com" style={{ width: '100%' }} 
-          value={credentials.email} 
-          onChangeText={(text) => setCredentials(prev => ({ ...prev, email: text }))} 
-          />
+      <SafeAreaView
+        style={{
+          backgroundColor: '#fff',
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          width: '100%',
+        }}
+      >
+        <View style={{ marginBottom: 40 }}>
+          <IconAt style={{ width: 80, height: 80 }} />
         </View>
 
-        <View style={{ width: '100%', gap: 10 }}>
-          <TextMalet style={{ fontSize: 16 }}>
-            Contraseña
-          </TextMalet>
-          <Input placeholder="********" style={{ width: '100%' }} 
-          value={credentials.password} 
-          onChangeText={(text) => setCredentials(prev => ({ ...prev, password: text }))} 
-          />
-        </View>
+        <View style={{ width: '85%', justifyContent: 'center', alignItems: 'center', gap: 20 }}>
+          <View style={{ width: '100%', gap: 10 }}>
+            <TextMalet style={{ fontSize: 16 }}>
+              Correo Electrónico
+            </TextMalet>
+            <Input placeholder="tucorreo@ejemplo.com" style={{ width: '100%' }}
+              value={credentials.email}
+              onChangeText={(text) => setCredentials(prev => ({ ...prev, email: text }))}
+            />
+          </View>
 
-        <View style={{ width: '100%', marginTop: 20, justifyContent: 'center', alignItems: 'center' }}>
-          {loading ? (
-            <ActivityIndicator size="small" color="#007AFF" />
-          ) : (
-            <Button text="Iniciar Sesión" onPress={handleSubmit} style={{ width: '100%' }} />
-          )}
-        </View>
+          <View style={{ width: '100%', gap: 10 }}>
+            <TextMalet style={{ fontSize: 16 }}>
+              Contraseña
+            </TextMalet>
+            <Input placeholder="********" style={{ width: '100%' }}
+              value={credentials.password}
+              onChangeText={(text) => setCredentials(prev => ({ ...prev, password: text }))}
+            />
+          </View>
 
-        <TouchableOpacity onPress={() => {router.push('/auth/register')}} style={{ 
-          marginTop: 20, 
-          flexDirection: 'row', 
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%', 
-          gap: 10 
+          <View style={{ width: '100%', marginTop: 20, justifyContent: 'center', alignItems: 'center' }}>
+            {loading ? (
+              <ActivityIndicator size="small" color="#007AFF" />
+            ) : (
+              <Button text="Iniciar Sesión" onPress={handleSubmit} style={{ width: '100%' }} />
+            )}
+          </View>
+
+          <TouchableOpacity onPress={() => { router.push('/auth/register') }} style={{
+            marginTop: 20,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            gap: 10
           }}>
-          <IconWallet style={{ width: 24, height: 24 }} />
-          <TextMalet style={{ fontSize: 16 }}>
-            Crea tu cuenta de Malet aquí!
-          </TextMalet>
-        </TouchableOpacity>
+            <IconWallet style={{ width: 24, height: 24 }} />
+            <TextMalet style={{ fontSize: 16 }}>
+              Crea tu cuenta de Malet aquí!
+            </TextMalet>
+          </TouchableOpacity>
 
-      </View>
-    </SafeAreaView>
+        </View>
+      </SafeAreaView>
     </>
   );
 }
